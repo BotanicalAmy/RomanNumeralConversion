f232400ac6c17b673db9fec41a07672595a0be4b
BotanicalAmy <amyfolkestad@gmail.com>
2022-11-17 19:39:18 -0700

    Added comments, realize I need to tweak this code because some numbers need to be subtracted...

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ class Solution:
         self.numeral = [self.roman_numeral_values[k] for k in self.numeral if k in self.roman_numeral_values]
         return sum(self.numeral)
 
+# I placed before V or X indicates one less,
+# so four is IV (one less than five) and nine is IX (one less than ten)
+# X placed before L or C indicates ten less,
+# so forty is XL (ten less than fifty) and ninety is XC (ten less than a hundred)
+# C placed before D or M indicates a hundred less, so four hundred is CD (a hundred less than five hundred)
+# and nine hundred is CM (a hundred less than a thousand)
 
+# name my solution class
 roman_solve = Solution()
 
+# ask the user to enter their roman numeral
 roman_numeral = input("Enter your roman numeral: \n")
-roman_numeral_length = len(roman_numeral)
+
+# get the value from the conversion method
 
 value_sum = roman_solve.romanToInt(roman_numeral)
 
